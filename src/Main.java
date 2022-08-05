92792185df002f191e3d22e40f93a3071acb7586
Sergey P <seregapanin23703@gmail.com>
2022-08-05 16:02:22 +0300

    Все 5 заданий дз

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        hw4Ex2();
+hw4Ex5();
     }
 
     public static void hw4Ex1() {
@@ -27,14 +27,69 @@ public class Main {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
 
+    }
+    public  static void hw4Ex3(){
+        int year = 400;
 
-
+        //boolean visokosnyYear=true ;
+        if (year%4==0 && year % 100 != 0|| year%400==0) {
+            System.out.println(year+" является высокосным");
+        }else {
+            System.out.println(year+" не является высокосным");
+        }
 
 
 
 
     }
+    public static void hw4Ex4(){
+        int deliveryDistant = 30;
+        int deliviryDays = 1;
+
+        if (deliveryDistant>20) {
+            deliviryDays++;
+        }
+        if (deliveryDistant>60) {
+            deliviryDays++;
+        }
+
+        System.out.println("Потребуется " +deliviryDays+   " дня");
 
 
+
+
+        //System.out.println("Потребуется дней ");
+    }
+    public static void hw4Ex5(){
+        int monthNumber = 5;
+        switch (monthNumber) {
+            case 12 :
+            case 1 :
+            case 2 :
+                System.out.println("Winter");
+                break;
+            case 3 :
+            case 4 :
+            case 5 :
+                System.out.println("Spring");
+                break;
+            case 6 :
+            case 7 :
+            case 8 :
+                System.out.println("Summer");
+                break;
+            case 9 :
+            case 10 :
+            case 11 :
+                System.out.println("Summer");
+                break;
+
+            default :
+                System.out.println("Такого месяца нет");
+
+
+        }
+
+    }
 
     }
