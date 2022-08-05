9332da191544e57143acf86347b10e0e0714ab07
Sergey P <seregapanin23703@gmail.com>
2022-08-05 10:25:35 +0300

    Домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-        int f = 4;
+        int age = 2;
+        if (age > 18) {
+            System.out.println("You should celebrate");
+            if (age < 21) {
+                System.out.println("Drink non alcohol beer");
+            }
+            else {
+                System.out.println("You can go everywhere");
+            };
+        }
+        else  if (age > 7 ) {
+                System.out.println("You need go to the school");
+            }
+            else {
+                System.out.println("go the kids club");
+        }
     }
-}
+
+    /*public static void hw4Ex1() {
+        int age = 21;
+        System.out.println("You can drive car");
+
+
+   */ }
