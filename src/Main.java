125faa6a50345799c157da07fbdd7361c8d51dd7
Sergey P <seregapanin23703@gmail.com>
2022-08-05 12:24:28 +0300

    Домашнее задание,упражнение 1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-
+     hw4Ex1();
     }
 
-    /*public static void hw4Ex1() {
-        int age = 21;
-        System.out.println("You can drive car");
+    public static void hw4Ex1() {
+
+        int clientIOS = 0;
+        if (clientIOS==0) {
+            System.out.println("Установите версию приложения для IOS по ссылке");}
+
+        if (clientIOS==1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
 
 
-   */ }
+
+    }
