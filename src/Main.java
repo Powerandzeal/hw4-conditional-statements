da97ab797fb31ae1a066c7d287362706ad9a68a5
Sergey P <seregapanin23703@gmail.com>
2022-08-05 13:22:57 +0300

    Домашнее задание,упражнение 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-     hw4Ex1();
+        hw4Ex2();
     }
 
     public static void hw4Ex1() {
@@ -12,6 +12,27 @@ public class Main {
         if (clientIOS==1) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+    }
+    public  static void hw4Ex2(){
+        int clientDeviseYear = 2013;
+        int OSClient= 1 ;
+        if (clientDeviseYear<2015&&OSClient==0) {
+            System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+        } if (clientDeviseYear>2015&&OSClient==0) {
+            System.out.println("Установите  версию приложения для IOS по ссылке");
+        }
+        if (clientDeviseYear<2015&&OSClient==1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }if (clientDeviseYear>2015&&OSClient==1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+
+
+
+
+
+
+
     }
 
 
